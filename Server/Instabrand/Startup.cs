9fe75a5947cc31ee77a656e98706ab2f66f107aa
ekleziast <ekleziast56@mail.ru>
2021-04-18 03:34:01 +0500

    Fix Server docker compose and logs

--- Server/Instabrand/Startup.cs
+++ Server/Instabrand/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using System.Net;
 using System.Text;
 using System.Text.Json.Serialization;
